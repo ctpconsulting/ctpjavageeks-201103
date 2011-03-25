7247c44efe8ee5ac5e402fb3e318578d4f1dfd0b
Daniel Käppeli <daniel.kaeppeli@ctp-consulting.com>
2011-03-25 22:44:33 +0100

    Initial Check In

---
+++ esb/source/src/main/java/ctp/book/rest/model/Book.java
@@ -0,0 +1,42 @@
+package ctp.book.rest.model;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "book")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class Book {
+
+	private String title;
+	private String description;
+	private Author author;
+	private String isbn;
+	
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public Author getAuthor() {
+		return author;
+	}
+	public void setAuthor(Author author) {
+		this.author = author;
+	}
+	public String getIsbn() {
+		return isbn;
+	}
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+	
+	
+}
