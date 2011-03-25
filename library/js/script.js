2a0d8870d7e008c0c86f502089b92cbbf476c860
unknown <smalaer@.ctp.com>
2011-03-26 00:07:01 +0100

    only data missing, the rest is now in there

--- library/js/script.js
+++ library/js/script.js
@@ -3,10 +3,35 @@ Stefan Malär
 */
 
 $(document).ready(function() {
-	$('#example').dataTable();
+	
+	//initialize data tabel
+	booksTable = $('#example').dataTable();
+	
+	//do some stuff with it
+	booksTable.fnClearTable();
+	booksTable.fnAddData( [[
+		"<a id='asdf' href='#'>testit</a>",
+		".2",
+		".3" 
+			],
+			[
+		"<a id='qwer' href='javascript:void(0);' onclick='add(this)'>testit 2</a>",
+		".3",
+		".34" 
+			] ]);
+	
+	
 } );
 
 
+function add(element){
+	$("#content").append("<div id='popup'>content</div>");
+	popup = $("#popup");
+	popup.css("left", $(element).offset().left).css("top",$(element).offset().top + 15);
+	$(popup).click(function(){
+		$("#popup").remove();
+	});
+}
 
 
 
