1fe57db42b047be204fb1541357b4d872e902558
Andreas Malaer <github@biteme.ch>
2011-03-26 03:33:38 +0100

    update retrieval and display add persistent local storage

--- library/js/books.js
+++ library/js/books.js
@@ -10,27 +10,101 @@ $.ajaxSetup({
         }
     });
 
-	   $.ajax( {
+	 var result =  $.ajax( {
 			type:'Get',
 			url:'http://thunder.local/test-data/books.xml',
-			success:parseXML,
-			error:function(data) {
-				/*$('#result').html("ERROR");*/
-				return "ERROR";
-			}
+			success:parseNewXML,
+			error:parsePersistentXML
 	  });
+	  
+	  //return result;
+	  
 }
   /*});
 });*/
 
+
+function parseNewXML(xml){
+	
+	localStorage["library.offline"] = xml;
+	
+	parseXML(xml);
+}
+
+function parsePersistentXML(xml){
+	
+	xml = localStorage["library.offline"];
+	alert(xml);
+	parseXML(xml);
+}
 
 function parseXML(xml) {
 	/*$(xml).find("tns:book").each(function(){
 			$('#result').append($(this).find("tns:title").text());
 		}
 	)*/
-	/*$('#result').append( $(xml).find('bookList').toArray());*/
-	return $(xml).find('bookList').toArray();
+	/*$('#main').append( $(xml).find('bookList').toArray());*/
+	/*
+	 objectArray = new Array();
+    	 $(xml).find('bookList').toArray().each(function(i){
+      rowObj = new Object();
+      rowObj.col1 = $(this).find('title').text();
+      rowObj.col2 = $(this).find('isbn').text();
+      rowObj.col3 = $(this).find('state').text();
+      temp = new Array();
+      temp = [
+		"testit 1",
+		"isbn123",
+		".3" ,
+		".4" ,
+		".5"  ,
+		".6" 
+			];
+			alert();
+      objectArray.push(temp);
+	});
+	return objectArray;*/
+
+	
+
+	objectArray = new Array();
+	
+	$(xml).find("book").each(function (i) {
+
+        //$('#main').append('<div class="book"><div class="title">' + $(this).find("title").text() + '</div><div class="description">' + $(this).find("description").text() + '</div>');
+
+	 	$('#main').append('<div class="book2">'+objectArray+'</div>');
+	 	temp = new Array();
+	 	temp = [
+		$(this).find("title").text(),
+		$(this).find("isbn").text(),
+		$(this).find("author").text() ,
+		$(this).find("description").text(),
+		$(this).find("state").text()  ,
+		$(this).find("borrower").text() 
+			] ;
+		addRow(temp);
+    });
+    
+	/*booksRaw = 	[[
+		"testit 1",
+		"isbn123",
+		".3" ,
+		".4" ,
+		".5"  ,
+		".6" 
+			] ,[
+		"testit 2",
+		"isbn456",
+		"2.3" ,
+		"2.4" ,
+		"2.5"  ,
+		"2.6" 
+			] ];
+	return booksRaw;*/
+	
+	
+	/*return $(xml).find('bookList').toArray();*/
 	/*
 	$(xml).find("book").each(function () {
 
