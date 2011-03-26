51ed2edaad962a3a8a4b7a086c70c2d98f5f22d8
Bartosz Majsak <bartosz.majsak@gmail.com>
2011-03-26 11:07:42 +0100

    Green test!

--- book-scanner-test/src/com/ctp/library/android/test/OpenLibraryIsbnFetcherIntegrationTest.java
+++ book-scanner-test/src/com/ctp/library/android/test/OpenLibraryIsbnFetcherIntegrationTest.java
@@ -1,11 +1,10 @@
 package com.ctp.library.android.test;
 
-import android.util.Log;
+import junit.framework.TestCase;
 
 import com.ctp.library.android.domain.BookInfo;
+import com.ctp.library.android.service.BookFetcher;
 import com.ctp.library.android.service.OpenLibraryBookFetcher;
-
-import junit.framework.TestCase;
 
 public class OpenLibraryIsbnFetcherIntegrationTest extends TestCase {
 	
@@ -13,14 +12,12 @@ public class OpenLibraryIsbnFetcherIntegrationTest extends TestCase {
 	public void testShouldFetchBookInfoFromOpenLibraryForValidISBN() throws Exception {
 		// given
 		String isbn = "9780679790730";
-		OpenLibraryBookFetcher fetcher = new OpenLibraryBookFetcher();
+		BookFetcher fetcher = new OpenLibraryBookFetcher();
 
 		// when
 		BookInfo bookInfo = fetcher.fetch(isbn);
 		
 		// then
-		Log.i("TEST", "Test message info");
-		Log.d("TEST", "Test message debug");
 		assertEquals("", isbn, bookInfo.getIsbn());
 	}
 	
--- book-scanner/src/com/ctp/library/android/service/OpenLibraryBookFetcher.java
+++ book-scanner/src/com/ctp/library/android/service/OpenLibraryBookFetcher.java
@@ -1,8 +1,12 @@
 package com.ctp.library.android.service;
 
 import org.apache.http.HttpResponse;
+import org.apache.http.client.ResponseHandler;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import android.util.Log;
 
@@ -17,12 +21,40 @@ public class OpenLibraryBookFetcher implements BookFetcher {
 		DefaultHttpClient client = new DefaultHttpClient();
 		try {
 			HttpResponse response = client.execute(get);
-			result = new BookInfo(String.valueOf(response.getStatusLine().getStatusCode()));
+			ResponseHandler<String> responseHandler = new BasicResponseHandler();
+			if (200 == response.getStatusLine().getStatusCode()) {
+				result = BookInfoResponse.fromJSON(responseHandler.handleResponse(response));
+			}
 		} catch (Exception e) {
 			Log.d("OpenLibraryBookFetcher", "Exception has been thrown", e);
 		}
 		return result ;
 	}
 	
+	
+	private static final class BookInfoResponse extends JSONObject {
+
+		private final String isbn;
+
+		public BookInfoResponse(String response) throws JSONException {
+			super(extractJson(response));
+			this.isbn = extractISBN(); 
+		}
+
+		private String extractISBN() {
+			String isbnKey = (String) keys().next();
+			return isbnKey.substring(isbnKey.indexOf(":") + 1);
+		}
+
+		private static String extractJson(String json) {
+			return json.substring(json.indexOf("{"));
+		}
+		
+		public static BookInfo fromJSON(String json) throws JSONException {
+			BookInfoResponse bookInfoResponse = new BookInfoResponse(json);
+			return new BookInfo(bookInfoResponse.isbn);
+		}
+
+	}
 
 }
