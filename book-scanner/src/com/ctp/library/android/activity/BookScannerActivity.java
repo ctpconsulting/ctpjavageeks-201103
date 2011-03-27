f0dd04ab1db48f5af99a0ce90507280b3725677f
Bartosz Majsak <bartosz.majsak@gmail.com>
2011-03-27 17:39:31 +0200

    Introduced dialog for displaying book's details.

--- book-scanner/src/com/ctp/library/android/activity/BookScannerActivity.java
+++ book-scanner/src/com/ctp/library/android/activity/BookScannerActivity.java
@@ -1,47 +1,36 @@
 package com.ctp.library.android.activity;
 
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
-
 import android.app.Activity;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.ctp.library.android.R;
 import com.ctp.library.android.domain.BookInfo;
-import com.ctp.library.android.domain.BookInfo.CoverSize;
 import com.ctp.library.android.service.BookFetcher;
 import com.ctp.library.android.service.OpenLibraryBookFetcher;
+import com.ctp.library.android.ui.BookDetailsDialog;
 
 public class BookScannerActivity extends Activity {
 	
 	private TextView text;
 	
-	private ImageView image;
-	
-	BookFetcher bookFetcher;
+	private BookFetcher bookFetcher;
 	
 	private BookInfo bookInfo;
+	
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 		bookFetcher = new OpenLibraryBookFetcher();
-		bindWidgets();
 		restoreIfBookAlreadyFetched();
 
 	}
 
 	public void scanButtonClickHandler(View view) {
-		bindWidgets();
 		Intent intent = ZxingIntentFactory.scanBarcode();
 		startActivityForResult(intent, 0);
 	}
@@ -51,52 +40,31 @@ public class BookScannerActivity extends Activity {
 		if (requestCode == 0) {
 			if (resultCode == RESULT_OK) {
 				String scanResult = intent.getStringExtra("SCAN_RESULT");
-				OpenLibraryBookFetcher bookFetcher = new OpenLibraryBookFetcher();
-				bookInfo = bookFetcher.fetch(scanResult);
-				presentBookDetails(bookInfo);
+				loadBook(scanResult);
 			} else if (resultCode == RESULT_CANCELED) {
 				text.setText("RESULT CANCELED");
 			}
 		}
 	}
 
+	private void loadBook(String isbn) {
+		bookInfo = bookFetcher.fetch(isbn);
+		showBook(bookInfo);
+	}
+
+	
+	private void showBook(BookInfo bookInfo) {
+		BookDetailsDialog bookDetailsDialog = new BookDetailsDialog(BookScannerActivity.this);
+		bookDetailsDialog.bindDetails(bookInfo);
+		bookDetailsDialog.show();
+	}
+
 	private void restoreIfBookAlreadyFetched() {
 		Object lastNonConfigurationInstance = getLastNonConfigurationInstance();
 		if (null != lastNonConfigurationInstance) {
 			bookInfo = (BookInfo) lastNonConfigurationInstance;
-			presentBookDetails(bookInfo);
+			showBook(bookInfo);
 		}
-	}
-	
-	private void presentBookDetails(BookInfo bookInfo) {
-		Bitmap coverImage = downloadImage(bookInfo.getCoverUrl(CoverSize.MEDIUM));
-		text.setText(bookInfo.getTitle());
-		image.setImageBitmap(coverImage);
-	}
-	
-	private Bitmap downloadImage(String imageLocation) {
-		Bitmap bitmap = null;
-		HttpURLConnection connection = null;
-		try {
-			URL imageUrl = new URL(imageLocation);
-			connection = (HttpURLConnection) imageUrl.openConnection();
-			connection.setDoInput(true);
-			connection.connect();
-			InputStream is = connection.getInputStream();
-			bitmap = BitmapFactory.decodeStream(is);
-		} catch (Exception e) {
-			Log.d("Book-scanner", "Exception while trying to download image from location " + imageLocation, e);
-		} finally {
-			if (null != connection) {
-				connection.disconnect();
-			}
-		}
-		return bitmap;
-	}
-
-	private void bindWidgets() {
-		text = (TextView) findViewById(R.id.content);
-		image = (ImageView) findViewById(R.id.imageView);
 	}
 
 	@Override
@@ -107,4 +75,3 @@ public class BookScannerActivity extends Activity {
 		return super.onRetainNonConfigurationInstance();
 	}
 }
-
---
+++ book-scanner/src/com/ctp/library/android/ui/BookDetailsDialog.java
@@ -0,0 +1,78 @@
+package com.ctp.library.android.ui;
+
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.ctp.library.android.R;
+import com.ctp.library.android.domain.BookInfo;
+import com.ctp.library.android.domain.BookInfo.CoverSize;
+
+public class BookDetailsDialog {
+
+	private final Dialog dialog;
+	
+	public BookDetailsDialog(Context context) {
+		this.dialog = new Dialog(context);
+        dialog.setContentView(R.layout.bookdialog);
+        dialog.setCancelable(true);
+        defineControls();
+	}
+
+	private void defineControls() {
+		Button button = (Button) dialog.findViewById(R.id.closeBookDetailsButton);
+        button.setOnClickListener(new OnClickListener() {
+        	@Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+	}
+
+	public void bindDetails(BookInfo bookInfo) {
+		dialog.setTitle(bookInfo.getTitle());
+		
+		TextView text = (TextView) dialog.findViewById(R.id.content);
+        ImageView image = (ImageView) dialog.findViewById(R.id.imageView);
+        
+        Bitmap coverImage = downloadImage(bookInfo.getCoverUrl(CoverSize.MEDIUM));
+        text.setText(bookInfo.getTitle());
+        image.setImageBitmap(coverImage);
+	}
+	
+	public void show() {
+		dialog.show();
+	}
+
+	private Bitmap downloadImage(String imageLocation) {
+		Bitmap bitmap = null;
+		HttpURLConnection connection = null;
+		try {
+			URL imageUrl = new URL(imageLocation);
+			connection = (HttpURLConnection) imageUrl.openConnection();
+			connection.setDoInput(true);
+			connection.connect();
+			InputStream is = connection.getInputStream();
+			bitmap = BitmapFactory.decodeStream(is);
+		} catch (Exception e) {
+			Log.d("Book-scanner", "Exception while trying to download image from location " + imageLocation, e);
+		} finally {
+			if (null != connection) {
+				connection.disconnect();
+			}
+		}
+		return bitmap;
+	}
+	
+}
